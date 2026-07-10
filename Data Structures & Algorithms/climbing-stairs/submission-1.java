a4deef330d43f87e640b5f4ae2d2393416833a23
Vanshika12verma <149817797+Vanshika12verma@users.noreply.github.com>
2026-07-10 17:32:01 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,50 @@
+/*
+//MEMOIZATION
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        for (int i=0; i<=n; i++) {
+            dp[i] = -1;
+        }
+        return solve(n,dp);
+    }
+    private int solve(int n, int[] dp) {
+        if (n<=1) return 1;
+        dp[n] = solve(n-1,dp) + solve(n-2,dp);
+        return dp[n];
+    }
+}
+*/
+//TABULATION
+class Solution {
+    public int climbStairs(int n) {
+        if (n<=1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
